932a0da7b88c8bc547fe381d0af83542ac570cff
Sergio Rodriguez <rodriguezmsergio@gmail.com>
2020-05-19 23:05:43 -0300

    Agregado de toolbar

--- Codigo/app/src/main/java/com/example/midiendodistanciasmobile/RegisterActivity.java
+++ Codigo/app/src/main/java/com/example/midiendodistanciasmobile/RegisterActivity.java
@@ -1,15 +1,27 @@
 package com.example.midiendodistanciasmobile;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class RegisterActivity extends Activity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class RegisterActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        showToolbar(getResources().getString(R.string.create_account), true);
+
+    }
+
+    public void showToolbar(String title, boolean upButton ){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }

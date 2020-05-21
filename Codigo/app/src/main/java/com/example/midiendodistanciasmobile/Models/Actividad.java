7a4514712eb6bb14d08e27be265c6cf4557a3fc6
Micaela Frias <mfrias@circostudio.com>
2020-05-21 18:12:39 -0300

    Agregado de datos a listview Actividades

--- Codigo/app/src/main/java/com/example/midiendodistanciasmobile/Models/Actividad.java
+++ Codigo/app/src/main/java/com/example/midiendodistanciasmobile/Models/Actividad.java
@@ -6,8 +6,23 @@ import java.util.Date;
 public class Actividad {
 
     public int Id;
-    public String Descripcion;
+    public int CantidadPasos;
     public Date Fecha;
     public Usuario Usuario;
+
+    public Actividad(int id, int cantidadPasos, Date fecha, com.example.midiendodistanciasmobile.Models.Usuario usuario) {
+        Id = id;
+        CantidadPasos = cantidadPasos;
+        Fecha = fecha;
+        Usuario = usuario;
+    }
+    public  Actividad(){
+
+    }
+
+    public String toString()
+    {
+        return "Nro: " +  Id + "Fecha: " + Fecha.getDay() + "/" + Fecha.getMonth() + "/" + Fecha.getYear()  + "Cantidad de pasos: " +  CantidadPasos;
+    }
 }
 

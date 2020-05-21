c176332787e078bca1ad6f7755a5b4da762a0e3c
Sergio Rodriguez <rodriguezmsergio@gmail.com>
2020-05-21 18:55:53 -0300

    Fix home activity, login activity

--- Codigo/app/src/main/java/com/example/midiendodistanciasmobile/Models/Salida.java
+++ Codigo/app/src/main/java/com/example/midiendodistanciasmobile/Models/Salida.java
@@ -3,4 +3,9 @@ package com.example.midiendodistanciasmobile.Models;
 public class Salida {
 
     public int Id;
+    public Date fecha;
+    public Time horaSalida;
+    public Time horaRegreso;
+    public float distanciaMax;
+    public float distanciaRecorrida;
 }

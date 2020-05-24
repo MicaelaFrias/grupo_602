18aa6c49afd6e6ea6fb4b1a6f321d4ea95428910
Sergio Rodriguez <rodriguezmsergio@gmail.com>
2020-05-24 19:13:29 -0300

    Feature Petición API Rest de registro

---
+++ Codigo/app/src/main/java/com/example/midiendodistanciasmobile/Constants/Constants.java
@@ -0,0 +1,18 @@
+package com.example.midiendodistanciasmobile.Constants;
+
+public class Constants {
+
+    public static final String ENV_TEST = "TEST";
+    public static final String ENV_DEV = "DEV";
+
+    public static final String URI_REGISTER = "http://so-unlam.net.ar/api/api/register";
+    public static final String URI_LOGIN = "http://so-unlam.net.ar/api/api/log";
+    public static final String URI_EVENTS = "http://so-unlam.net.ar/api/api/event";
+
+    public static final String STATE_SUCCESS = "success";
+    public static final String STATE_ERROR = "error";
+    public static final Integer NUM_COMMISSION = 2;
+    public static final Integer NUM_GROUP = 602;
+
+
+}
--- Codigo/app/src/main/java/com/example/midiendodistanciasmobile/RegisterActivity.java
+++ Codigo/app/src/main/java/com/example/midiendodistanciasmobile/RegisterActivity.java
@@ -1,30 +1,100 @@
 package com.example.midiendodistanciasmobile;
 
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.midiendodistanciasmobile.Constants.Constants;
+import com.example.midiendodistanciasmobile.WebService.AsyncResponse;
+import com.example.midiendodistanciasmobile.WebService.PeticionAPIRest;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class RegisterActivity extends AppCompatActivity {
 
     Button registerButton;
+
+    TextInputEditText name;
+    TextInputEditText lastname;
+    TextInputEditText dni;
+    TextInputEditText email;
+    TextInputEditText password;
+
+    String valueName;
+    String valueLastname;
+    Integer valueDni;
+    String valueEmail;
+    String valuePassword;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         showToolbar(getResources().getString(R.string.create_account), true);
-        //Navegación a mainActivity
+
         registerButton = findViewById(R.id.registerButton);
+        name = findViewById(R.id.name);
+        lastname = findViewById(R.id.lastname);
+        dni = findViewById(R.id.dni);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
 
+                try {
+
+                    if ( name.getText().toString().length() == 0 || lastname.getText().toString().length() == 0 ||
+                            dni.getText().toString().length() == 0 || email.getText().toString().length() == 0 ||
+                            password.getText().toString().length() == 0){
+                        Log.i("Inputs Error", "onClick: Error en valores");
+                        return;
+                    }
+
+                    if ( password.getText().toString().length() < 8){
+                        Log.i("Password Error", "onClick: Password Menor a 8 caracteres");
+                        return;
+                    }
+
+                    valueName = name.getText().toString();
+                    valueLastname =  lastname.getText().toString();
+                    valueDni =  Integer.parseInt(dni.getText().toString());
+                    valueEmail =  email.getText().toString();
+                    valuePassword = password.getText().toString();
+
+                    PeticionAPIRest registro =  new PeticionAPIRest(valueName, valueLastname, valueDni, valueEmail, valuePassword, Constants.URI_REGISTER,
+                        new AsyncResponse() {
+                            @Override
+                            public void processFinish(String status, String env, String token) {
+
+                                if (status == Constants.STATE_ERROR) {
+                                    Log.e("ERROR", "processFinish: Error en petición rest API." );
+                                    return;
+                                }
+
+                                Log.i("Response", "State: " + status);
+                                Log.i("Response", "Env: " + env);
+
+                                if (env == Constants.ENV_DEV )  {
+                                    Log.i("Response", "Token: " + token);
+                                }
+
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                    });
+
+                   registro.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+                }catch (Exception e) {
+                    Log.e("Register Error", "onClick: Error" + e.getMessage(),e);
+                }
             }
         });
     }
